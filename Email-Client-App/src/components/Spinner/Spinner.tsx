a01a628de52f5b0e37a4efadcccffea4135fb277
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2023-01-15 22:05:45 +0530

    Added Spinner Component

---
+++ Email-Client-App/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import classes from './Spinner.module.css';
+
+const Spinner:FC = () => {
+    return(
+        <div className={classes.loader}>Loading...</div>
+    )
+}
+export default Spinner;
